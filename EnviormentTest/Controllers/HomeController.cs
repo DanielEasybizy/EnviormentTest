187e160f1f593a5e600b7ae2c59b13f7cd3b1ca7
tomer@easybizy.net <tomer@easybizy.net>
2014-08-24 17:06:25 +0300

    Testing commit

--- EnviormentTest/Controllers/HomeController.cs
+++ EnviormentTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EnviormentTest.Controllers
     {
         public ActionResult Index()
         {
-            /*This is a test notes!.*/
+            /*This is a test notes!. Edit them and push!*/
             return View();
         }
     }
