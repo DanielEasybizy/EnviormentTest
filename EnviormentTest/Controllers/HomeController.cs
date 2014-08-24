604c0fa56bae98d6e132c8f8ea26562adb14882d
tomer@easybizy.net <tomer@easybizy.net>
2014-08-24 16:28:32 +0300

    Stam

--- EnviormentTest/Controllers/HomeController.cs
+++ EnviormentTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EnviormentTest.Controllers
     {
         public ActionResult Index()
         {
-            /*This is a test notes!.dddd*/
+            /*This is a test notes!.*/
             return View();
         }
     }
